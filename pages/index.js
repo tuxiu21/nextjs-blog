66e539e83c2c10f338636a1bed1d4af37544987a
tuxiu21 <zhangyufan709@gmail.com>
2023-05-14 00:25:52 +0800

    change intor

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({allPostsData}){
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[self intro!!!]</p>
+        <p>[你好，欢迎来到本网站。]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
